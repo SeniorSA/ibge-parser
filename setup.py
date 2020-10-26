723a9eec101ce0b1ebf54dd0a78ecc6f7b286539
Flávio Omar Losada <flavio.losada@senior.com.br>
2020-10-26 11:14:31 -0300

    Adding logzero as a dependency of ibge parser

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open(file="README.md", mode="r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ibge-parser",
-    version="1.0.1",
+    version="1.0.2",
     author="Senior Sistemas - Senior Labs",
     author_email="research@senior.com.br",
     description="IBGE Parser is a Python library to get microdata from IBGE - (Instituto Brasileiro de Geografia e Estatística) census and convert the data to readable CSV files.",
@@ -17,6 +17,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+      'logzero==1.5.0'
     ],
     python_requires='>=3.6',
 )
